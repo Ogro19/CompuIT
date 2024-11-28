233832ba20a8fe4c38611f68751e3c9baed13bfe
Ogro19 <brandonvicencio0711@gmail.com>
2024-11-27 18:22:46 -0800

    mejora que no se refleja2

--- login.php
+++ login.php
@@ -11,6 +11,7 @@
     <div class="login-container">
         <!-- Logo -->
         <div class="logo-wrapper">
+            <img src="assets/img/logo.jpg" alt="CompuIT Logo" class="logo-image">
         </div>
         
         <h1>Iniciar Sesión</h1>
