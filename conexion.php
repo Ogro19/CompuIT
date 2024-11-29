239ccaa5fac40badd9d4b4c175beaa1223a14b34
ulisesmonge <ulisesmonge40@gmail.com>
2024-11-29 02:39:12 +0000

    Corregido el método pdo

--- conexion.php
+++ conexion.php
@@ -16,6 +16,7 @@ try {
     // Crear conexión utilizando PDO
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
 } catch (PDOException $e) {
     die("Error al conectar a la base de datos: " . $e->getMessage());
 }
