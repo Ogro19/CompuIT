9c58703491f6bed1d2f90027ba6c749a8eef93ed
Ogro19 <brandonvicencio0711@gmail.com>
2024-12-02 16:50:15 -0800

    cambios 3 ya estoy hasta la verga

--- admin_respuestas.php
+++ admin_respuestas.php
@@ -89,7 +89,6 @@
                 <tbody>
                     <?php
 
-// Incluir la conexión a la base de datos
 include('conexion.php');
                     // Consulta de la tabla contactanos
                     $stmtContactos = $pdo->query("SELECT id, nombre, apellido, email, telefono, empresa, problema, fecha FROM contactanos");
